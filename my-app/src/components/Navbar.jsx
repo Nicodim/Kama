850d2db1f6e70ab1b7019fa1c895843588a75393
Nick <nicky.kravtsov@gmail.com>
2019-10-13 16:06:48 +0300

    add commit

--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Navbar = () => {
     return <nav className='main-nav'>
         <ul className="nav__list">
-            <li><a>Profile</a></li>
-            <li><a>massage</a></li>
-            <li><a>news</a></li>
-            <li><a>music</a></li>
-            <li><a>settings</a></li>
+            <li className='nav__option'><a>Profile</a></li>
+            <li className='nav__option'><a>massage</a></li>
+            <li className='nav__option'><a>news</a></li>
+            <li className='nav__option'><a>music</a></li>
+            <li className='nav__option'><a>settings</a></li>
         </ul>
     </nav>
 }
