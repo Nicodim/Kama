d138ff85c29b0428fa27b27510a9c4c3f959527b
Nick <nicky.kravtsov@gmail.com>
2019-10-25 19:14:23 +0300

    les 26

--- my-app/src/components/Dialogs/Dialogs.jsx
+++ my-app/src/components/Dialogs/Dialogs.jsx
@@ -24,6 +24,7 @@ const Dialogs = (props) => {
         {id: 5, name: 'Boi'}
 
     ]
+    let dialogElement = dialogsData.map(d =>  <DialogItem name={d.name} id={d.id}/>);
 
     let MassageData = [
         {id: 1, massage: 'Ti cho pes??'},
@@ -33,19 +34,23 @@ const Dialogs = (props) => {
         {id: 5, massage: 'taki da'}
 
     ]
+let massagesElement = MassageData.map(m => <Massage massage={m.massage} id={m.id}/> );
+    
     return (
         <section className={s.dialogs}>
             <div className={s.content__list}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[4].id}/>
+                { dialogElement }
+                {/*<DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>*/}
+                {/*<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>*/}
+                {/*<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>*/}
+                {/*<DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>*/}
+                {/*<DialogItem name={dialogsData[3].name} id={dialogsData[4].id}/>*/}
             </div>
             <div className={s.massage__list}>
-                <Massage massage={MassageData[0].massage} id={MassageData[0].id}/>
-                <Massage massage={MassageData[2].massage} id={MassageData[2].id}/>
-                <Massage massage={MassageData[3].massage} id={MassageData[3].id}/>
+                { massagesElement }
+                {/*<Massage massage={MassageData[0].massage} id={MassageData[0].id}/>*/}
+                {/*<Massage massage={MassageData[2].massage} id={MassageData[2].id}/>*/}
+                {/*<Massage massage={MassageData[3].massage} id={MassageData[3].id}/>*/}
             </div>
 
         </section>
